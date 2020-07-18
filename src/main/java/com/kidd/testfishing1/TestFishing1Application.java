cf86714f4306e580a7224233c472fd7bc9efbd83
kiddvs <435303131@qq.com>
2020-07-18 17:55:33 +0800

    Initial commit

---
+++ src/main/java/com/kidd/testfishing1/TestFishing1Application.java
@@ -0,0 +1,13 @@
+package com.kidd.testfishing1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFishing1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestFishing1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kidd/testfishing1/TestFishing1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kidd.testfishing1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFishing1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
