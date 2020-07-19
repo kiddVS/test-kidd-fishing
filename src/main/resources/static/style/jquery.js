b87df3832063b010a0970196a23a4b40e2903def
kiddvs <435303131@qq.com>
2020-07-19 21:07:55 +0800

    feat：init2

--- src/main/resources/static/style/jquery.js
+++ src/main/resources/static/style/jquery.js
@@ -1,5 +1,5 @@
 ! function(a) {
-    "function" == typeof define && define.amd ? define(["jquery"], a) : "object" == typeof module && module.exports ? module.exports = a(require("jquery")) : a(jQuery)
+    "function" == typeof define && define.amd ? define(["static/style/jquery"], a) : "object" == typeof module && module.exports ? module.exports = a(require("static/style/jquery")) : a(jQuery)
 }(function(a) {
     a.extend(a.fn, {
         validate: function(b) {
