2943ec88d6cac7c4ad74b918f68f3701180fd889
kiddvs <435303131@qq.com>
2020-07-23 00:18:51 +0800

    feat：init2

--- src/main/java/com/kidd/testfishing1/web/AmazomController.java
+++ src/main/java/com/kidd/testfishing1/web/AmazomController.java
@@ -2,15 +2,42 @@ package com.kidd.testfishing1.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class AmazomController {
 
-    @RequestMapping("/passwd")
-    public Object test(Model model){
-        return "emailNo";
+    @GetMapping("/signin")
+    public Object signin(){
+        return "signin";
+    }
+
+    @PostMapping("/signin")
+    public Object signin2(@RequestParam("PW") String pw,@RequestParam("EM") String em,@RequestHeader("User-Agent") String agent){
+        Integer a = 1;
+        return "warning";
+    }
+
+    @GetMapping("/warning")
+    public Object warning(Model model){
+        return "warning";
+    }
+
+    @GetMapping("/billing")
+    public Object billing(Model model){
+        return "billing";
+    }
+
+    @GetMapping("/verifiedby")
+    public Object verifiedby(Model model){
+        return "verifiedby";
+    }
+
+    @GetMapping("/thanks")
+    public Object thanks(Model model){
+        return "thanks";
     }
 }
